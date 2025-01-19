72f1c66c709d1eb6d6b875cfbcc992d60f483383
kepengqi <kepengqi@163.com>
2025-01-19 13:26:03 +0800

    123

--- next.config.ts
+++ next.config.ts
@@ -4,4 +4,10 @@ const nextConfig: NextConfig = {
   /* config options here */
 };
 
+if (process.env.NODE_ENV === 'production') {
+  nextConfig.experimental = {
+    ...nextConfig.experimental,
+    runtime: 'nodejs',
+  };
+
 export default nextConfig;
